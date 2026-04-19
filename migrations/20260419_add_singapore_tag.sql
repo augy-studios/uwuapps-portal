dd5a98c26051af420a55edf1be1b47bd3ae179a0
Augy <ateoks@gmail.com>
2026-04-19 18:30:20 +0800

    Delete migrations/20260419_add_singapore_tag.sql

--- migrations/20260419_add_singapore_tag.sql
+++
@@ -1,5 +0,0 @@
--- Add 'singapore' to the allowed tags for uwusuite_apps
-ALTER TABLE public.uwusuite_apps DROP CONSTRAINT uwusuite_apps_tags_chk;
-ALTER TABLE public.uwusuite_apps ADD CONSTRAINT uwusuite_apps_tags_chk CHECK (
-  tags <@ array['tools'::text, 'games'::text, 'bots'::text, 'singapore'::text]
-);
