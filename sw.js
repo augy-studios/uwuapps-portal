96cb44c41902d331340eed1208df1f2e2144f22a
Augy <ateoks@gmail.com>
2026-07-31 17:59:49 +0800

    Update brand color tokens for improved theme support and increment cache version to v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "uwusuite-v8";
+const CACHE_VERSION = "uwusuite-v9";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const OFFLINE_URL = "/offline.html";
